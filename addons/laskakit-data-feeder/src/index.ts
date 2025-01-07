f71a7f5c5ad436a425edfb5a730ea46bfef94d52
Radoslav Irha <radoslav.irha@gmail.com>
2025-01-07 20:09:00 +0100

    initial commit

---
+++ addons/laskakit-data-feeder/src/index.ts
@@ -0,0 +1,49 @@
+import { $log } from '@tsed/logger';
+import { ConfigProvider, ConfigProviderOptions } from '@radoslavirha/tsed-configuration';
+import { Platform, ServerConfiguration } from '@radoslavirha/tsed-platform';
+import { Server } from './Server.js';
+import { ConfigModel } from './models/index.js';
+
+const SIG_EVENTS = [
+    'beforeExit',
+    'SIGHUP',
+    'SIGINT',
+    'SIGQUIT',
+    'SIGILL',
+    'SIGTRAP',
+    'SIGABRT',
+    'SIGBUS',
+    'SIGFPE',
+    'SIGUSR1',
+    'SIGSEGV',
+    'SIGUSR2',
+    'SIGTERM'
+];
+
+try {
+    const config = new ConfigProvider(<ConfigProviderOptions>{
+        service: 'LaslaKit Data Feeder',
+        fallbackPort: 4000,
+        configModel: ConfigModel
+    }, true);
+    
+    const configuration: ServerConfiguration = {
+        ...config.server,
+        api: config.api
+    };
+
+    const platform = await Platform.bootstrap(Server, configuration);
+    await platform.listen();
+
+    SIG_EVENTS.forEach((evt) => process.on(evt, () => platform.stop()));
+
+    ['uncaughtException', 'unhandledRejection'].forEach((evt) =>
+        process.on(evt, async (error) => {
+            $log.error({ event: 'SERVER_' + evt.toUpperCase(), message: error.message, stack: error.stack });
+            await platform.stop();
+        })
+    );
+} catch (error) {
+    $log.error({ event: 'SERVER_BOOTSTRAP_ERROR', message: error.message, stack: error.stack });
+}
+
---
+++ docs/README.md
@@ -0,0 +1,33 @@
+# Documentation
+
+## Development
+
+This document describes the process for running this application on your local computer.
+
+### Prerequisites
+
+- node.js > 20.18.1
+- pnpm > 9.14.4
+
+### Getting started
+
+Create `.env` and add your PAT for GiHub packages. You'll need to use it before installing like: `NODE_AUTH_TOKEN=XXX pnpm install`
+
+## Docker
+
+### Local build
+
+Create `.npmrc.docker` in root. This is just `.npmrc` with replaced env variable with auth token for private npm packages.
+
+Use following command to build image locally `docker build -t {image}:{tag} --secret id=npmrc,src=.npmrc.docker ./addons/{addon}`.
+
+Create `./addons/{addon}/options.json` representing options passed to addon from Home assistant (declared in config.json[options]).
+
+Use following command to run container locally `docker run {image}:{tag} -v ./addons/{addon}/options.json:/data/options.json`.
+
+Example:
+
+```sh
+docker build -t radoslavirha/ha-addon-laskakit-data-feeder:0.0.1 --secret id=npmrc,src=.npmrc.docker ./addons/laskakit-data-feeder
+docker run -v ./addons/laskakit-data-feeder/options.json:/data/options.json radoslavirha/ha-addon-laskakit-data-feeder:0.0.1
+```
