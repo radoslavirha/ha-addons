bb9ceb4799619e40d87f2163c2da1b8939a6842a
Radoslav Irha <radoslav.irha@gmail.com>
2025-01-07 20:23:57 +0100

    initial commit

---
+++ addons/laskakit-data-feeder/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bashio
+
+# cat /data/options.json
+
+# export LASKAKIT_URL="$(bashio::config 'laskakit_url')"
+# export IMAGE_TO_CONSOLE=$(bashio::config 'image_to_console')
+
+# echo $LASKAKIT_URL
+# echo $IMAGE_TO_CONSOLE
+
+bashio::log.info "Starting Homeassistant Add-on LaskaKit data feeder."
+pnpm start:prod
